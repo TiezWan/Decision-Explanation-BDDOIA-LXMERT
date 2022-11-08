8a4125f7d878f0bfe951017dcd5f7438d5fdb202
Adrien <wantiez.adrien@gmail.com>
2022-11-08 11:28:56 +0000

    Commented a few lines to make code runnable

--- src_DecExp/DecExp.py
+++ src_DecExp/DecExp.py
@@ -503,7 +503,7 @@ if __name__ == "__main__":
     #     print("cannot set start method to 'spawn'")
     #     sys.exit()
     # Build Class
-    args.img_num=100
+    args.img_num=None
     args.train='train'
     args.valid='val'
     args.test=None
